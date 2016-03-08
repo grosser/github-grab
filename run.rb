011e663589990cd194e5a20d7185edc788888d4f
Michael Grosser <michael@grosser.it>
2016-03-07 20:22:18 -0800

    initial

---
+++ run.rb
@@ -0,0 +1,15 @@
+require "bundler/organization_audit/repo.rb"
+
+token = ARGV[0]
+
+repos = Bundler::OrganizationAudit::Repo.all(:organization => "zendesk", :token => token)
+puts "#{repos.size} repos"
+
+names = repos.select do |repo|
+  puts repo.project
+  content = repo.content(".travis.yml") rescue nil
+  puts content
+  content.to_s[/code_climate/]
+end.map(&:project)
+
+puts names
